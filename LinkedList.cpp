0eb24ba662329329bf94f8a498af65e2f0a0dc02
vtorres4803 <vtorres4803@sdsu.edu>
2026-04-17 20:52:56 -0700

    advanced linkedlist.cpp

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -1,1 +1,16 @@
 #include "LinkedList.h"
+namespace LinkedListNS 
+{
+  template<class T>
+  void headInsert(Node<T>*& head, const T& theData) 
+  {
+    head = new Node<T>(theData, head);
+  }
+  template<class T>
+  void insert(Node<T>* afterMe, const T& theData) 
+  {
+    if (afterMe == nullptr) return;
+    Node<T>* temp = new Node<T>(theData, afterMe->getLink());
+    afterMe->setLink(temp);
+  }
+}
