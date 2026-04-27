78f160a782890c0f42b07ba0aba485ba43465676
vtorres4803 <vtorres4803@sdsu.edu>
2026-04-26 23:47:19 -0700

    LinkedList.cpp

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -1,55 +1,73 @@
 #include "LinkedList.h"
-namespace LinkedListNS 
-{
-// Add node at the head
-  template<class T>
-  void headInsert(Node<T>*& head, const T& theData) 
-  {
-    head = new Node<T>(theData, head);
-  }
-// Add node at the head
 
-  template<class T>
-  void insert(Node<T>* afterMe, const T& theData) 
-  {
-    if (afterMe == nullptr) return;
+namespace LinkedListNS
+{
+
+// Add node at the head
+template<class T>
+void headInsert(Node<T>*& head, const T& theData)
+{
+    head = new Node<T>(theData, head);
+}
+
+// Add node after another node
+template<class T>
+void insert(Node<T>* afterMe, const T& theData)
+{
+    if (afterMe == nullptr)
+    {
+        return;
+    }
+
     Node<T>* temp = new Node<T>(theData, afterMe->getLink());
     afterMe->setLink(temp);
-  }
+}
+
 // Delete node after 'before'
-  template<class T>
-  void deleteNode(Node<T>* before) 
-  {
-    if (before == nullptr || before->getLink() == nullptr) 
-      return;
-    
+template<class T>
+void deleteNode(Node<T>* before)
+{
+    if (before == nullptr || before->getLink() == nullptr)
+    {
+        return;
+    }
+
     Node<T>* temp = before->getLink();
     before->setLink(temp->getLink());
     delete temp;
-  }
+}
+
 // Delete first head
-  template<class T>
-  void deleteFirstNode(Node<T>*& head) 
-  {
-    if (head == nullptr) 
-      return;
+template<class T>
+void deleteFirstNode(Node<T>*& head)
+{
+    if (head == nullptr)
+    {
+        return;
+    }
 
     Node<T>* temp = head;
     head = head->getLink();
     delete temp;
-  }
-  // Search for a value
-  template<class T>
-  Node<T>* search(Node<T>* head, const T& target) 
-  {
+}
+
+// Search for a value
+template<class T>
+Node<T>* search(Node<T>* head, const T& target)
+{
     Node<T>* current = head;
 
-    while (current != nullptr) 
+    while (current != nullptr)
     {
         if (current->getData() == target)
+        {
             return current;
+        }
 
         current = current->getLink();
     }
+
     return nullptr;
+}
+
 }
